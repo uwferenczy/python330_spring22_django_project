1cda3a8f66450772f349d1ff29d2b7da6680b0a1
uwferenczy <ferenczy@uw.edu>
2022-06-14 17:11:32 -0700

    Updated heroku.py

--- mysite/heroku.py
+++ mysite/heroku.py
@@ -14,4 +14,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 ALLOWED_HOSTS = ["*"]
 
-MIDDLEWARE = ("whitenoise.middlewear.WhiteNoiseMiddelware", *MIDDLEWARE)
+MIDDLEWARE = (
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    *MIDDLEWARE,
+)
